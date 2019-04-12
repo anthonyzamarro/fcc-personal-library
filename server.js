dd345e02cedc32c62b51aa4273df3414ec025d08
Anthony Zamarro <anthonyzamarro@Anthonys-MacBook-Air.local>
2019-04-11 20:23:21 -0400

    deploy correctly:

--- server.js
+++ server.js
@@ -28,16 +28,16 @@ app.use(helmet.noCache());
 app.use(helmet.hidePoweredBy({setTo: 'PHP 4.2.0'}));
 
 //Index page (static HTML)
-// app.use(express.static("build"));
+app.use(express.static("build"));
 
-// app.route('/')
-//   .get(function (req, res) {
-//     res.sendFile(process.cwd() + '/client/public/index.html');
-//   });
+app.route('/')
+  .get(function (req, res) {
+    res.sendFile(process.cwd() + '/client/public/index.html');
+  });
 
 // https://coursework.vschool.io/deploying-mern-with-heroku/
 // deploy to Heroku
-app.use(express.static(path.join(__dirname, "client", "build")))
+// app.use(express.static(path.join(__dirname, "client", "build")))
 
 //For FCC testing purposes
 fccTestingRoutes(app);
@@ -55,9 +55,9 @@ app.use(function(req, res, next) {
 
 // https://coursework.vschool.io/deploying-mern-with-heroku/
 // deploy to Heroku
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-})
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+// })
 
 
 
