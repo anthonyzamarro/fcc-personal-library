4d6cfbd0c0c195b5286c1dc075cd58098f0affa5
Anthony Zamarro <anthonyzamarro@Anthonys-MacBook-Air.local>
2019-04-11 21:38:17 -0400

    fixing deploy build

--- server.js
+++ server.js
@@ -65,10 +65,10 @@ app.listen(process.env.PORT || 3000, function () {
   } 
   else if (process.env.NODE_ENV === 'production') {
     // set static folder
-    app.use(express.static('client/build'))
+    app.use(express.static('client/dist'))
 
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+      res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
     });
   }
 });
